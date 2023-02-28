5d98f89f10c2ecd614abbe8ba8e69d2c83b49209
Giselle Ruybal <giselle.ruybal@gmail.com>
2023-02-27 22:49:19 -0300

    Formulario y alerta

--- src/components/ItemCount/ItemCount.jsx
+++ src/components/ItemCount/ItemCount.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react"
-export const ItemCount = ({ValInicial, stock}) => {
+export const ItemCount = ({ValInicial, stock, onAdd}) => {
 
   const [contador, setContador] = useState(ValInicial) //Defino un estado con valor inicial 1
   
@@ -12,7 +12,7 @@ export const ItemCount = ({ValInicial, stock}) => {
       <button className="btn btn-dark" onClick={()=> restar()}>-</button> 
       {contador} 
       <button className="btn btn-dark" onClick={()=> sumar()}>+</button>
-      <button className="btn btn-light">Agregar al Carrito</button>
+      <button className="btn btn-light" onClick={()=> onAdd(contador)}>Agregar al Carrito</button>
     </div>
   )
 }
